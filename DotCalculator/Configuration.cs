9dd28acae47e1a82375c924f88f3102b4e5b1370
Noevain <noevain@gmail.com>
2025-01-10 02:38:57 +0100

    Renaming blabla

--- DotCalculator/Configuration.cs
+++ DotCalculator/Configuration.cs
@@ -2,7 +2,7 @@
 using Dalamud.Plugin;
 using System;
 
-namespace SamplePlugin;
+namespace DotCalculator;
 
 [Serializable]
 public class Configuration : IPluginConfiguration
--- DotCalculator/Windows/ConfigWindow.cs
+++ DotCalculator/Windows/ConfigWindow.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 using Dalamud.Interface.Windowing;
 using ImGuiNET;
 
-namespace SamplePlugin.Windows;
+namespace DotCalculator.Windows;
 
 public class ConfigWindow : Window, IDisposable
 {
